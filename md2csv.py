baf144271ebb89acbedb7ef98967473326351d47
Steve Grogorick <grogorick@cg.cs.tu-bs.de>
2022-08-03 15:25:26 +0200

    added question type enum (for now only supporting `kprim`) to allow other question types in the future

--- md2csv.py
+++ md2csv.py
@@ -1,14 +1,22 @@
 import csv
 from dataclasses import dataclass
+from enum import Enum
 import re
 import sys
 from typing import List
+
+
+class Type(Enum):
+    kprim = 'kprim'
+
+    def fromString(string):
 
 
 @dataclass
 class Question:
     dir: str = ''
     question: str = ''
+    type: Type|None = None
     rfs: str = ''
     answers: List[str]|None = None
 
@@ -46,26 +54,37 @@ for line_no, line in enumerate(lines):
         questions.append(current_question := Question())
         current_question.dir = '###'.join(current_dir)
 
-    elif current_question.answers is None and not line.startswith('-'):
+    elif current_question.type is None and not line.startswith('>'):
         if current_question.question == '' and line.strip() == '':
             continue
         line = re.sub(r'\*\*([^*]+)\*\*', r'<b>\1</b>', line.strip())
         line = re.sub(r'\*([^*]+)\*', r'<i>\1</i>', line.strip())
         current_question.question += line + '<br />'
 
-    else:
+    elif line.startswith('>'):
         if current_question.question == '':
-            error('Question required before listing answers.')
-        if current_question.answers is None:
-            current_question.answers = []
-        if line.startswith('-'):
-            if len(current_question.answers) >= 4:
-                error('No more than 4 answers allowed.')
-            line_tmp = line[1:].strip()
-            if line_tmp[0] not in 'rf':
-                error('Answers must start with either `- r ` or `- f `\n       e.g.: `- r This answer is right.` or `- f This answer is false.`')
-            current_question.rfs += {'r':'1', 'f':'0'}[line_tmp[0]] + '|'
-            current_question.answers.append(line_tmp[1:].strip())
+            error('Question text required before specifying the question type.')
+        if current_question.type is not None:
+            error('Question type already set.')
+        current_question.type = { 'kprim': Type.kprim }[line[1:].strip()]
+
+    else:
+        if current_question.type is None:
+            error('Question type required before listing answers.')
+
+        if current_question.type == Type.kprim:
+            if current_question.answers is None:
+                current_question.answers = []
+            if line.startswith('-'):
+                if len(current_question.answers) >= 4:
+                    error('No more than 4 answers allowed.')
+                line_tmp = line[1:].strip()
+                if line_tmp[0] not in 'rf':
+                    error('Answers must start with either `- r ` or `- f `\n       e.g.: `- r This answer is right.` or `- f This answer is false.`')
+                current_question.rfs += {'r':'1', 'f':'0'}[line_tmp[0]] + '|'
+                current_question.answers.append(line_tmp[1:].strip())
+            else:
+                current_question.answers[-1]
 
 
 print(f'Found {len(questions)} questions.\nGenerating `{filename}.csv` ...')
