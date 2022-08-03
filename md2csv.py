fdd83778928d54955596729069df7ac41a7766bd
Steve Grogorick <grogorick@cg.cs.tu-bs.de>
2022-08-03 15:12:16 +0200

    more parsing

--- md2csv.py
+++ md2csv.py
@@ -1,15 +1,16 @@
 import csv
 from dataclasses import dataclass
+import re
 import sys
 from typing import List
 
 
 @dataclass
 class Question:
-    dir: str
-    question: str
-    rfs: List[str]
-    answers: List[str]
+    dir: str = ''
+    question: str = ''
+    answers: List[str]|None = None
+    rfs: List[str] = []
 
 
 if len(sys.argv) < 2:
@@ -20,24 +21,28 @@ with open(sys.argv[1]) as file:
     lines = file.readlines()
 
 
-questions : List[Question] = []
-dir: List[str] = []
+questions : List[Question] = [Question()]
+current_dir: List[str] = []
+current_question: Question = questions[0]
 
 for line in lines:
     if line.startswith('#'):
-        if dir:
-            questions.append(Question(dir, question, rfs, answers))
-        dir = line[1:].strip()
-        question = ''
-        rfs = []
-        answers = []
-    if line.startswith('>'):
-        question += line[1:].strip() + '<br />'
-    if line.startswith('-'):
-        line = line[1:].strip()
-        rfs.append('1' if line[0] == 'r' else '0')
-        answers.append(line[1:].strip())
-questions.append(Question(dir, question, rfs, answers))
+        questions.append(current_question := Question())
+
+        level = re.match(r'#+[^#]', line).end()
+        current_dir = current_dir[:level - 1] + [line[level:].strip()]
+        current_question.dir = '###'.join(current_dir)
+
+    elif current_question.answers is None and not line.startswith('-'):
+        current_question.question += line[1:].strip() + '<br />'
+
+    else:
+        if current_question.answers is None:
+            current_question.answers = []
+        if line.startswith('-'):
+            line = line[1:].strip()
+            current_question.rfs.append('1' if line[0] == 'r' else ('0' if line[0] == 'f' else None))
+            current_question.answers.append(line[1:].strip())
 
 
 with open(sys.argv[1] + '.csv', 'w', newline='') as csvFile:
