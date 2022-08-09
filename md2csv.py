a6cf4e088147cd07a8afdc21e47c43d8016b0402
Steve Grogorick <grogorick@cg.cs.tu-bs.de>
2022-08-09 12:33:35 +0200

    no question type prefix for per-question options

--- md2csv.py
+++ md2csv.py
@@ -65,34 +65,41 @@ for line_no, (line, last_line) in enumerate(zip(lines, [''] + lines[:-1])):
         print(f'\nERROR: {msg}\nin: {dir}\nline {line_no+1}: {line}')
         exit()
 
-    def parse_option(line: str):
+    def parse_option(line: str, question_type: Type|None = None):
         for key in options:
-            key_str = f'*{key}*'
+            if question_type is None:
+                key_str = f'*{key}*'
+            elif key.startswith(question_type.value + '_'):
+                key_str = f'*{key[len(question_type.value) + 1:]}*'
+            else:
+                continue
             if line.startswith(key_str):
                 return key, line[len(key_str):].strip()
         return None, None
 
     # global options at the beginning of the file
-    if len(current_dir) == 0:
+    if len(current_dir) == 0 and line.startswith('*'):
         key, value = parse_option(line)
         if key is not None:
             options[key] = value
             # print(f'==> {key}: {value}')
-            continue
+        continue
 
     # comments
     if line.startswith('```'):
         is_comment = not is_comment
         continue
-    if is_comment:
+    elif is_comment:
         # print('==> skip comment line')
         continue
 
-    if line.strip() == '---':
+    # horizontal line to separate questions
+    elif line.strip() == '---':
         # print('==> skip horizontal line')
         continue
 
-    if line_empty and current_question is None:
+    # empty line
+    elif line_empty and current_question is None:
         # print('==> skip empty line')
         continue
 
@@ -112,13 +119,16 @@ for line_no, (line, last_line) in enumerate(zip(lines, [''] + lines[:-1])):
             current_question.type = Type.fromString(line[1:].strip())
             current_question.options = options.copy()
             # print('==> type ' + current_question.type.name + ' | dir ' + str(current_question.dir))
+
+        # per question options
         else:
-            key, value = parse_option(line[1:].lstrip())
+            key, value = parse_option(line[1:].lstrip(), current_question.type)
             if key is not None:
                 current_question.options[key] = value
                 # print(f'==> {key}: {value}')
                 continue
 
+    # empty line
     elif line_empty and current_question.question == '':
         # print('==> skip empty line')
         continue
@@ -144,7 +154,7 @@ for line_no, (line, last_line) in enumerate(zip(lines, [''] + lines[:-1])):
                 if line_tmp[0] not in 'rf':
                     error('Answers must start with either `- r ` or `- f `\n       e.g.: `- r This answer is right.` or `- f This answer is false.`')
                 if len(current_question.answers) == 0:
-                    current_question.question += current_question.options['kprim_append']
+                    current_question.question = current_question.question.strip() + '\n' + current_question.options['kprim_append']
                 current_question.rfs += {'r':'1', 'f':'0'}[line_tmp[0]] + '|'
                 current_question.answers.append(line_tmp[1:].lstrip())
             else:
